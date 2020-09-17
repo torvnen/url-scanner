c7f1019f52c2b03ff672e9fff799625dbb2db882
Niko Torvinen <torvnen@gmail.com>
2020-09-17 18:14:20 +0300

    Domain extension finder: created and passed unit test

---
+++ tools/Torvnen.UrlScanner.Tools.DomainExtensionFinder/DomainExtensionFinder.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+
+namespace Torvnen.UrlScanner.Tools.DomainExtensionFinder
+{
+    /// <summary>
+    /// To scan a string for URLs, we must know what kind of domain extensions are available.
+    /// This class' responsibility is to provide a list of available domain extensions.
+    /// </summary>
+    public class DomainExtensionFinder
+    {
+        private readonly HttpClient _client;
+
+        public DomainExtensionFinder()
+        {
+            _client = new HttpClient();
+        }
+
+        /// <summary>
+        /// Gets a list of available top-level domains from IANA's data site.
+        /// </summary>
+        /// <returns>
+        /// Asynchronous list of domain names. Most likely in all capital letters.
+        /// </returns>
+        public async IAsyncEnumerable<string> GetTopLevelDomainsAsync()
+        {
+            var rawTlds = await _client.GetStringAsync("https://data.iana.org/TLD/tlds-alpha-by-domain.txt");
+
+            using var reader = new StringReader(rawTlds);
+            string line;
+            while ((line = await reader.ReadLineAsync()) != null)
+            {
+                // The file might have "comments" that start with #. Skip those.
+                if (line.StartsWith("#")) continue;
+                yield return line;
+            }
+        }
+    }
+}
---
+++ tools/Torvnen.UrlScanner.Tools.DomainExtensionFinder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Torvnen.UrlScanner.Tools.DomainExtensionFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
