722029ac6c0786e9fd7d3a84c403a895f2504a18
Niko Torvinen <torvnen@gmail.com>
2020-09-17 17:07:59 +0300

    Ping test passed

---
+++ Torvnen.UrlScanner/src/Torvnen.UrlScanner/Controllers/PingController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Torvnen.UrlScanner.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PingController : ControllerBase
+    {
+        private readonly ILogger<PingController> _logger;
+
+        public PingController(ILogger<PingController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Used to check availability of the server.
+        /// </summary>
+        /// <returns>"pong"</returns>
+        [HttpGet]
+        public string Ping()
+        {
+            var callerIpAddress = Request.HttpContext.Connection.RemoteIpAddress;
+
+            _logger.LogTrace("Pinged by {callerIpAddress}", callerIpAddress);
+
+            return "pong";
+        }
+    }
+}
---
+++ Torvnen.UrlScanner/tests/Torvnen.UrlScanner.Tests.Integration/PingTest.cs
@@ -0,0 +1,31 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using NUnit.Framework;
+using Torvnen.UrlScanner.Api;
+
+namespace Torvnen.UrlScanner.Tests.Integration
+{
+    public class PingTest
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public PingTest()
+        {
+            _factory = new WebApplicationFactory<Startup>();
+        }
+
+        [Test]
+        public async Task Get_Ping_ShouldReturnPong()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetStringAsync("/ping");
+
+            // Assert
+            Assert.NotNull(response);
+            Assert.True(response.ToLower().Contains("pong"));
+        }
+    }
+}
